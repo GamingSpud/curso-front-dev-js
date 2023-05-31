36b09c774645dc8088bf7fb0653ed30cf8b13346
GamingSpud <decortesa@outlook.com>
2023-05-31 00:31:19 -0500

    Added mobile menu and restricted both menus from showing in incorrect view

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileBurger = document.querySelector('.menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+mobileBurger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');}
+);
+
+
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
